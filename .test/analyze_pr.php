c7040cceb3e36e283b8bb15528d67fd93cfe8b59
MarcinFr <m.frymark@alekseon.com>
2025-03-06 20:37:39 +0100

    test

--- .test/analyze_pr.php
+++ .test/analyze_pr.php
@@ -12,7 +12,7 @@ $jiraApiToken = getenv("JIRA_API_TOKEN"); // Token API Jira
 
 $headers = [
     "Authorization: token $githubToken",
-    "Accept: application/vnd.github.v3+json"
+    "Accept: application/vnd.github.v3+json",
     "User-Agent: Analyze PR Application"
 ];
 
