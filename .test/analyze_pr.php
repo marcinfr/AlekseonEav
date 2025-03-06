a96e5680b2a4ff7ae2694a6474afd930beb3814c
MarcinFr <m.frymark@alekseon.com>
2025-03-06 22:18:54 +0100

    test

--- .test/analyze_pr.php
+++ .test/analyze_pr.php
@@ -19,7 +19,7 @@ $headers = [
 // Pobranie zmienionych plików w PR
 $url = "https://api.github.com/repos/$repo/pulls/$prNumber/files";
 
-$url = "https://github.com/Alekseon/magento2-widget-forms/pull/24/files";
+$url = "https://api.github.com/repos/Alekseon/magento2-widget-forms/pulls/24/files";
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
