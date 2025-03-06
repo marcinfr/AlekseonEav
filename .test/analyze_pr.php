30c3fec342571b00061e9d06a6dc98a84eb5b10e
MarcinFr <m.frymark@alekseon.com>
2025-03-06 21:23:55 +0100

    test

--- .test/analyze_pr.php
+++ .test/analyze_pr.php
@@ -54,7 +54,7 @@ Podaj konkretne problemy i sposoby ich naprawy.
 
 Zmiany w kodzie:\n\n" . implode("\n\n", $changedFiles);
 
-$prompt = iconv('UTF-8', 'UTF-8//IGNORE', utf8_encode($prompt));
+$prompt = mb_convert_encoding($prompt, 'UTF-8', 'auto');
 
 $data = [
     "model" => "gpt-4",
